c5003c0e588396f06791e6ab02dd84c778398163
MarcosFranco <MarcosFranco@Marcos>
2019-08-23 09:53:33 -0500

    Cambié M por Marios

--- Hola/src/hola/Hola.java
+++ Hola/src/hola/Hola.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hola;
 
 /**
@@ -17,7 +12,7 @@ public class Hola {
     public static void main(String[] args) {
         // TODO code application logic here
         for (int i = 0; i < 10; i++) {
-            System.out.println("M");
+            System.out.println("Marios");
         }
     }
     
