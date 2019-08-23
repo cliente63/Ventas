411b3688c3da6d5ce66b6e1dfae0a4bc910fd2aa
MarcosFranco <MarcosFranco@Marcos>
2019-08-23 09:54:53 -0500

    Cambié M por Marios2

--- Hola/src/hola/Hola.java
+++ Hola/src/hola/Hola.java
@@ -12,7 +12,7 @@ public class Hola {
     public static void main(String[] args) {
         // TODO code application logic here
         for (int i = 0; i < 10; i++) {
-            System.out.println("Marios");
+            System.out.println("Marios2");
         }
     }
     
